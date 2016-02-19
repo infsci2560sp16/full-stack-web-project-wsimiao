2624cb68d090559fb692590da133f7fab3b166c0
wsimiao <siw20@pitt.edu>
2016-02-18 19:01:36 -0500

    change java file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,21 +25,14 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> {
-            RelativisticModel.select();
+    get("/hello", (req, res) -> "Hello World");
 
-            String energy = System.getenv().get("ENERGY");
+    // get("/", (request, response) -> {
+    //         Map<String, Object> attributes = new HashMap<>();
+    //         attributes.put("message", "Hello World!");
 
-            Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
-            return "E=mc^2: " + energy + " = " + m.toString();
-        });
-
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
-
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
+    //         return new ModelAndView(attributes, "index.ftl");
+    //     }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
       Connection connection = null;
