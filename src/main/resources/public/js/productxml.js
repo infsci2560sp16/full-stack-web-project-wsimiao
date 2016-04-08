cc86cfc27f75534fe43c2a901f15606ead9aca17
wsimiao <siw20@pitt.edu>
2016-04-08 00:42:43 -0400

    xml

---
+++ src/main/resources/public/js/productxml.js
@@ -0,0 +1,20 @@
+$(function() {
+	$.ajax({
+		url : "/skinstore/getItemsXML",
+    type : "get",
+    dataType : "XML",
+		success : handleDataXML
+});
+}
+
+function handleDataXML(data) {
+  $(data).find('Item').each(function(){
+    var itemid = $(this).find('itemid').text();
+    var itemname = $(this).find('itemname').text();
+    var itembrand = $(this).find('itembrand').text();
+    var itemcategory = $(this).find('itemcategory').text();
+    var itemcolor = $(this).find('itemcolor').text();
+    $(table).find(tbody).append('<td>' + itemid + '</td><td>' + itemname + '</td><td>' + itembrand + '</td><td>' + itemcategory + '</td><td>' + itemcolor + '</td>');
+
+  });
+}
