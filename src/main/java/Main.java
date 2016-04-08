df6b5de05085688b3e9b087024775cb5f4cdc19e
wsimiao <siw20@pitt.edu>
2016-04-08 02:16:11 -0400

    try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,24 +26,24 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    //Apply headers across all routers for CORS
-    CorsFilter.apply();
-
-    //Fix CORS when posting by adding options respone for Preflight check
-    options("/*", (request,response)->{
-
-        String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
-        if (accessControlRequestHeaders != null) {
-            response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-        }
-
-        String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
-        if(accessControlRequestMethod != null){
-            response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
-        }
-
-        return "OK";
-    });//end options
+    // //Apply headers across all routers for CORS
+    // CorsFilter.apply();
+    //
+    // //Fix CORS when posting by adding options respone for Preflight check
+    // options("/*", (request,response)->{
+    //
+    //     String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+    //     if (accessControlRequestHeaders != null) {
+    //         response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+    //     }
+    //
+    //     String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+    //     if(accessControlRequestMethod != null){
+    //         response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+    //     }
+    //
+    //     return "OK";
+    // });//end options
 
     Object r = new ItemRoutes();
     Object r2 = new ItemRender();
--- src/main/resources/public/js/productxml.js
+++ src/main/resources/public/js/productxml.js
@@ -1,43 +1,3 @@
-// $(function() {
-// 	$.ajax({
-// 		url : "/skinstore/getItemsXML",
-//     type : "get",
-//     dataType : "XML",
-// 		success : handleDataXML
-// });
-// });
-/*
-function getDataXML(){
-  alert("hell1");
-  $.ajax({
-		url : "/skinstore/getItemsXML",
-    type : "get",
-    dataType : "XML",
-		success : handleDataXML
-  });
-}
-
-function handleDataXML(data){
-  alert("hell2");
-  $(data).find('Item').each(function(){
-    var itemid = $(this).find('itemid').text();
-    var itemprice = $(this).find('itemprice').text();
-    var itemname = $(this).find('itemname').text();
-    var itembrand = $(this).find('itembrand').text();
-    var itemcategory = $(this).find('itemcategory').text();
-    var itemdescription = $(this).find('itemdescription').text();
-    var itemcolor = $(this).find('itemcolor').text();
-    var itemlove = $(this).find('itemlove').text();
-    var itemstock = $(this).find('itemstock').text();
-    var itemgender = $(this).find('itemgender').text();
-    var itemsize = $(this).find('itemsize').text();
-    var itemimg = $(this).find('itemimg').text();
-    //$(table).find(tbody).append('<td>' + itemid + '</td><td>' + itemname + '</td><td>' + itembrand + '</td><td>' + itemcategory + '</td><td>' + itemcolor + '</td>');
-    //$("div.itemsxml").append('<p> item name' + itemname + '</p>');
-     $('<tr></tr>').html('<td>'+itemid+'</td><td>'+ itemprice + '</td><td>' +itemname+'</td><td>'+itembrand+'</td><td>'+ itemcategory +'</td><td>'+ itemdescription + '</td><td>'+ itemcolor +'</td><td>'+ itemlove +'</td><td>'+ itemstock +'</td><td>'+ itemgender +'</td><td>'+ itemsize +'</td><td>'+itemimg +'</td>').appendTo('#itemtable');
-  });
-}
-*/
 $(function() {
 	$.ajax({
 		url : "/skinstore/getItemsXML",
@@ -58,7 +18,7 @@ $(function() {
         var itemsize = $(this).find('itemsize').text();
         var itemimg = $(this).find('itemimg').text();
         $('<tr></tr>').html('<td>'+itemid+'</td><td>'+ itemprice + '</td><td>' +itemname+'</td><td>'+itembrand+'</td><td>'+ itemcategory +'</td><td>'+ itemdescription + '</td><td>'+ itemcolor +'</td><td>'+ itemlove +'</td><td>'+ itemstock +'</td><td>'+ itemgender +'</td><td>'+ itemsize +'</td><td>'+itemimg +'</td>').appendTo('#itemtable');
-      }); // end of inner function
+      });
 		}
 	});
 });
